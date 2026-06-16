01292289d554130a4578d389eca5542f305d19cf
Siddhi Manoj Gangan <gangansm@rknec.edu>
2026-06-16 19:28:11 +0530

    Time: 16 ms (23.47%) | Memory: 20.3 MB (16.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,9 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-
-        for char in t:
-            if char in s:
-                s = s.replace(char,"",1)
-        return len(s) == 0
+        return sorted(s) == sorted(t)
