cc1570ed3c8f0c1c3bd645e08a982a9b31f99d90
Siddhi Manoj Gangan <gangansm@rknec.edu>
2026-06-21 18:17:58 +0530

    Time: 15 ms (14.80%) | Memory: 19.1 MB (99.20%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,12 +1,11 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        
         res = ""
         for i in words:
             val = 0
             for j in range(len(i)):
-                pos = ord(i[j].lower()) - ord('a') +1
-                val+= weights[pos-1]
+                pos = ord(i[j].lower()) - ord('a')
+                val+= weights[pos]
             val = val % 26
             letter = chr(97 + ((26 - val) - 1))
             res += letter
