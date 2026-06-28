b0a78e59cfc6cbeb7c774c7fd27ecf3ec6adab7e
Siddhi Manoj Gangan <gangansm@rknec.edu>
2026-06-28 19:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (27.40%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        list_s = s.split(" ")
+        # print(list_s)
+        if len(pattern) != len(list_s):
+            return False
+        map_ptos = {}
+        map_stop = {}
+
+        for x,y in zip(pattern,list_s):
+            if(x in map_ptos):
+                if(map_ptos[x] != y):
+                    return False
+            else:
+                map_ptos[x] = y
+            if(y in map_stop):
+                if(map_stop[y] != x):
+                    return False
+            else:
+                map_stop[y] = x
+        print(map_ptos)
+        return True
